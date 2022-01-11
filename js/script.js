685953e1b4d13527d32721c6d6f9aa027b2cb552
rahul <rahulloni000@gmail.com>
2022-01-11 22:49:45 +0545

    variable operators datatypes String method

---
+++ js/script.js
@@ -0,0 +1,79 @@
+// alert("Hello!");
+//console.log(prompt("What is Your Name ?")); 
+console.log("I am Rahul Kumar");
+console.error("I am an error");
+console.warn("I am a Warning");
+console.info("I am a info ");
+
+//Variable
+// var let and const
+
+
+var x= 5;
+let y= 6;
+const z= 7;
+
+
+//y = 8;
+
+
+if(true){
+    let y=5;
+    var x= 6
+    console.log(x);
+    console.log(y);
+}
+console.log(x);
+    console.log(y);
+console.log(z);
+
+
+
+// Operators
+
+// console.log(2 + 2);
+// console.log(2 * 3);
+// console.log(10 - 5);
+// console.log(10 / 5);
+
+//let n=10;
+//console.log(--n);
+//console.log(++n);
+
+
+
+
+//DataType(String, Number , boolean ,   null , undifined , symbol )
+
+
+let name="Rahul Kumar";
+let age =26;
+let isProgrammer= true;
+let noValue=null;
+let unAssigned;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isProgrammer);
+console.log(typeof noValue);
+console.log(typeof 6)
+console.log(typeof Rahul);
+
+
+
+
+// String Method 
+
+let firstName="Rahul ";
+let lastName="Kumar";
+let Laguage="HTML , CSS and JavaScripts ";
+
+
+console.log(firstName);
+console.log(firstName+lastName);
+console.log("my first name is " + firstName +"last name is " + lastName);
+
+
+
+
+
